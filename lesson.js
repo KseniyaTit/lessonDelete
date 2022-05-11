a1747608134b952d25e57b2640a5f4475e8ec106
KseniyaTit <inessa134@gmail.com>
2022-05-11 21:01:35 +0300

    feat: added str

--- lesson.js
+++ lesson.js
@@ -1,1 +1,110 @@
-hgghfhgfh
+/*let arr = [1,2,3,4]
+function func(){
+    arr.shift
+    arr.unshift[0]=4
+    arr.pull
+    arr.push[3] = 1
+    return arr
+}
+
+*/
+
+/*
+function func(str){
+    let newStr = str.split("@")
+     return newStr.pop()
+ }
+ */
+/*
+ function func(...arr){
+     return arr
+ }
+ console.log(arr(1,2,3,4))
+
+ const a =[1,2]
+ const b =[3,4]
+ console.log([...a,...b])
+
+ const c =[...a]
+
+ Math.max
+
+ console.log(-0 === +0)/// true
+
+ Object.is(+0 -0)//false
+ Object.is (NaN, NaN)//true
+
+
+
+ *//
+ let a =["a","b","c"]
+ console.log(a =="abc")
+
+ /*.toString, +, String('') */
+ /*
+ console.log([] +"")
+
+ console.log(Math.floor()) -// округление в меньшую сторону
+ console.log(Math.cell()) // в большую
+ console.log(Math.round()) 
+ .toFixed()
+
+ Math.abs()
+
+ const newArray = new Array(2)
+ */
+a.fill('undefined')
+a.splice()
+
+////////////////////////////////////////////*/*
+/*const arr2 = [1,2,3,4]
+
+const arr1 =[7,8]
+
+console.log(arr2.concat(arr1))
+
+arr.include*/
+/*
+const array = [
+    {
+        name:"Artur",
+        age:18,
+    },
+    {
+        name:"Alex",
+        age:25,
+    }
+]
+console.log(array.find((item) => item.name === "Artur"))
+
+
+elemName.map(i, index)=>{
+    console.log(i)
+    return i
+}
+
+*/
+
+
+const newMas =[
+    {
+        name:"Bob",
+        age: 19,
+    },
+    {
+        name:"Mark",
+        age: 5,
+    },
+    {
+        name:"Alex",
+        age: 21,
+    }
+]
+console.log(newMas.filter(item => item.age>18))
+
+
+
+
+function arraywww (arr){
+arr.flat().filter(item => item===1).lenght
+return arr}
